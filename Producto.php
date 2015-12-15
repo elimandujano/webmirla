686ac3faca04b4aa3840327fbf6be03fb5011be5
Eli <Eli@192.168.1.38>
2015-12-14 23:31:54 -0500

    encapsulando los atributos jje

--- Producto.php
+++ Producto.php
@@ -4,7 +4,19 @@
 		/*Atributos de clase*/
 		private $descripcion = '';
 		private $cantidad = 0;
-
+            public function setDescripcion(){
+                $this->descripcion = $descripcion;
+                             
+            }
+            public function getDescripcion() {
+                return $this->descripcion;
+            }
+            public function setCantidad() {
+                $this->cantidad=$cantidad;
+            }
+            public function getCantidad() {
+                return $this->cantidad;
+            }
 	}
 
 
