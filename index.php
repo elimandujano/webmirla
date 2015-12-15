d5d1bcbf12ccbe8993e40b31686328c9975ef9a8
Elí Nelson Mandujano Condor <El Nelson Mandujano Condor>
2015-12-14 23:33:52 -0500

    encapsulando los atributos jeje

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        
+        
+    </head>
+    <body>
+        <?php
+            echo "hola mundo de elito";
+        ?>    
+        
+    </body>
+
+    <footer>
+        <h5>todos los derechos reservados </h5>
+        <a>http://localhost/wemirla/consultamodelos.php</a>
+        
+    </footer>
+    
+</html>    
